ed865d214b43f59a3a51b0fadb6385856ee3ab14
Tiago Maia <tiago.maia@trt6.jus.br>
2024-05-28 21:10:07 -0300

    Using externa validation for transfers

--- src/main/java/br/maia/ticopay/transfers/control/TransferAuthenticationException.java
+++ src/main/java/br/maia/ticopay/transfers/control/TransferAuthenticationException.java
@@ -4,4 +4,8 @@ public class TransferAuthenticationException extends RuntimeException {
     public TransferAuthenticationException(String message, Throwable cause) {
         super(message, cause);
     }
+
+    public TransferAuthenticationException(String message) {
+        super(message);
+    }
 }
--- src/main/java/br/maia/ticopay/transfers/control/TransfersStore.java
+++ src/main/java/br/maia/ticopay/transfers/control/TransfersStore.java
@@ -3,6 +3,7 @@ package br.maia.ticopay.transfers.control;
 import br.maia.ticopay.transfers.entity.Transfer;
 import br.maia.ticopay.wallet.control.CarteiraStore;
 import br.maia.ticopay.transfers.boundary.TransfersResource;
+import io.quarkus.security.UnauthorizedException;
 import jakarta.enterprise.context.Dependent;
 import jakarta.enterprise.event.Event;
 import jakarta.inject.Inject;
@@ -11,13 +12,17 @@ import jakarta.json.JsonObject;
 import jakarta.json.stream.JsonParser;
 import jakarta.transaction.Transactional;
 import jakarta.ws.rs.ServerErrorException;
+import jakarta.ws.rs.WebApplicationException;
+import org.eclipse.microprofile.faulttolerance.Retry;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.math.BigDecimal;
+import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.function.Predicate;
 
 @Dependent
 @Transactional(Transactional.TxType.MANDATORY)
@@ -42,23 +47,41 @@ public class TransfersStore {
         walletStore.depositar(payee, value);
         walletStore.sacar(payer, value);
         Transfer transfer = new Transfer(payer, payee, value);
+        validar(transfer);
         repository.persist(transfer);
         onTransfer.fire(new TransferEvent(payer, payee, value));
         return transfer;
     }
 
+    @Retry(abortOn = {WebApplicationException.class})
     void validar(Transfer transfer) {
         try (HttpClient client = HttpClient.newHttpClient()) {
-            HttpRequest request = HttpRequest.newBuilder().build();
+            HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://util.devi.tools/api/v2/authorize"))
+                .build();
             try {
-                HttpResponse<InputStream> response = client.send(request, HttpResponse.BodyHandlers.ofInputStream());
+                HttpResponse<InputStream> response =
+                    client.send(request, HttpResponse.BodyHandlers.ofInputStream());
                 JsonObject jsonObject = Json.createReader(response.body()).readObject();
-                System.out.println(response.statusCode());
-                System.out.println(jsonObject.toString());
-
+                switch (response.statusCode()) {
+                    case 200: {
+                        boolean valid = processValidation(jsonObject);
+                        if(!valid) {
+                            throw new WebApplicationException("Transfer not authorized", 401);
+                        }
+                        break;
+                    }
+                    case 401:
+                    case 403: {
+                        throw new WebApplicationException("Transfer not authorized", response.statusCode());
+                    }
+                    default: throw new ServerErrorException(
+                        "Transfer could not be authorized (http response: %d)".formatted(response.statusCode()), 500
+                    );
+                }
             } catch (IOException e) {
                 throw new TransferAuthenticationException(
-                    "An IOExcetion occurred when validating the transfer.", e
+                    "An IOException occurred when validating the transfer.", e
                 );
             } catch (InterruptedException e) {
                 throw new TransferAuthenticationException(
@@ -66,5 +89,13 @@ public class TransfersStore {
                 );
             }
         }
+    }
+
+    private static boolean processValidation(JsonObject jsonObject) {
+        Predicate<JsonObject> success =
+            (JsonObject j) -> "success".equals(j.getString("status"));
+        Predicate<JsonObject> authorized =
+            (JsonObject j) -> j.getJsonObject("data").getBoolean( "authorization");
+        return success.and(authorized).test(jsonObject);
     }
 }
